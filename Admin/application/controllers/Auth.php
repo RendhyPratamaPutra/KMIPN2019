96057cfaed6797d3b36080d3a7b1659b6c57764a
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-08-13 22:37:11 +0700

    n

--- Admin/application/controllers/Auth.php
+++ Admin/application/controllers/Auth.php
@@ -3,7 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
 
 	public function index()
 	{
